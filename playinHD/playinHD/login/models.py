09269d985007a06b4f542b60da36fa114fcc5596
yulanbo <1151629541@qq.com>
2019-05-13 22:26:02 +0800

    new

--- playinHD/playinHD/login/models.py
+++ playinHD/playinHD/login/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class user(models.Model):
-    AgentInfo_id=models.IntegerField(primary_key=True)
+class users(models.Model):
     userphone=models.CharField(max_length=50)
     username=models.CharField(max_length=50)
     password=models.CharField(max_length=50)
--- playinHD/playinHD/login/views.py
+++ playinHD/playinHD/login/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import users
 from django.shortcuts import render,redirect,reverse
 from django.http import HttpResponse
+from django.contrib import messages
+import json
 def mylogin(request):
     return render(request,'index.html')
 def register(request):
     return render(request,'register.html')
 def register_check(request):
-    return HttpResponse('asfdasdf')
+    userphone1=request.POST.get('username')
+    username=request.POST.get('myname')
+    password=request.POST.get('password')
+    ret = {"status": "NG", "msg": None}
+    if not users.objects.filter(userphone=userphone1):
+        users.objects.create(userphone=userphone1,username=username,password=password)
+        ret["status"]="NG"
+        ret["msg"]="注册成功！"
+        return HttpResponse(json.dumps(ret))
+    else:
+        ret["status"] = "QG"
+        ret["msg"] = "注册失败！"
+        return HttpResponse(json.dumps(ret))
+
+
+
+
+
+
